5ef199431e8176489e973174b172dedf7af6f281
Mateus Ryan <mthryan@protonmail.com>
2023-06-20 16:30:59 -0300

    ent
    Client skeleton

---
+++ Client/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '../app.css';
+</script>
+
+<slot />
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.slate.950);
+        color: theme(colors.slate.300);
+	}
+</style>
